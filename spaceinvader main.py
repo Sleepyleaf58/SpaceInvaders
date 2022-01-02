30976560991a4457aecfce6a1a67ff8458abe8ed
Sleepyleaf58 <frank.huang2@student.tdsb.on.ca>
2022-01-01 20:21:34 -0500

    Update spaceinvader main.py

--- spaceinvader main.py
+++ spaceinvader main.py
@@ -1,1 +1,19 @@
 import pygame
+
+#Initializes the code
+pygame.init()
+
+#Create the window
+screen = pygame.display.set_mode((800,600))
+
+#Title and Icon
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("")
+pygame.display.set_icon(icon)
+
+#Game loop
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
